4b0a77f5d4289e2304ba3263186812b0f43528bc
Derek Villars and Nick Mead <derek+nick@>
2017-02-09 15:36:32 -0800

    fix tries increment issue

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -18,7 +18,37 @@ namespace Hangman
             {
                 string input = Request.Form["new-answer"];
                 Game newGame = new Game(input);
+                Game.SetGameList(newGame);
                 return View["gameScreen.cshtml", newGame];
+            };
+
+            Post["/Guessed"] = _ =>
+            {
+                string guess = Request.Form["guess"];
+                List<Game> gameList = new List<Game>();
+                gameList = Game.GetGameList();
+                Game currentGame = gameList[0];
+                currentGame.LetterChecker(guess);
+                Game.SetGameList(currentGame);
+                return View["gameScreen.cshtml", currentGame];
+            };
+
+            Post["/Answer"] = _ =>
+            {
+                string wordGuess = Request.Form["word-guess"];
+                List<Game> gameList = new List<Game>();
+                gameList = Game.GetGameList();
+                Game currentGame = gameList[0];
+                if (currentGame.CheckWin(wordGuess))
+                {
+                    return View["win.cshtml"];
+                }
+                else
+                {
+                    currentGame.SetTries(currentGame.GetTries());
+                    Game.SetGameList(currentGame);
+                    return View["gameScreen.cshtml", currentGame];
+                }
             };
         }
     }
--- Objects/Game.cs
+++ Objects/Game.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Hangman.Objects
 {
@@ -10,6 +11,7 @@ namespace Hangman.Objects
         private int _tries;
         private string _result;
         private List<string> _knownLetters = new List<string>();
+        private static List<Game> _gamesList = new List<Game>();
 
         public string GetCurrentState()
         {
@@ -36,6 +38,7 @@ namespace Hangman.Objects
         public void SetTries(int newTries)
         {
             _tries = newTries;
+            _result = "You have executed " + _tries + " tries!";
         }
 
         public string GetResult()
@@ -56,6 +59,26 @@ namespace Hangman.Objects
             _knownLetters = newKnownLetters;
         }
 
+        public static List<Game> GetGameList ()
+        {
+            return _gamesList;
+        }
+        public static void SetGameList (Game currentGame)
+        {
+          if (_gamesList.Any())
+          {
+            _gamesList[0] = currentGame;
+          }
+          else
+          {
+            _gamesList.Add(currentGame);
+          }
+        }
+        public static void ClearGameList()
+        {
+            _gamesList.Clear();
+        }
+
         public Game (string answer)
         {
             _answer = answer;
@@ -65,20 +88,21 @@ namespace Hangman.Objects
             }
             _tries = 0;
             _result = "You have executed " + _tries + " tries!";
+            _currentState = ListToString();
         }
 
-        public void CheckLetter(string letter)
+        public void LetterChecker(string letter)
         {
             if (_answer.IndexOf(letter) > -1)
             {
                 for (int i = 0; i < _answer.Length; i++)
                 {
-                    if (_answer.Substring(i, i + 1) == letter)
+                    if (_answer.Substring(i, 1) == letter)
                     {
                         _knownLetters[i] = letter + " ";
                     }
                 }
-                _result = ListToString();
+                _currentState = ListToString();
             }
             else
             {
@@ -89,6 +113,7 @@ namespace Hangman.Objects
 
         public string ListToString ()
         {
+            _currentState = "";
             for (int i = 0; i < _answer.Length; i++)
             {
                 _currentState += _knownLetters[i];
@@ -96,5 +121,19 @@ namespace Hangman.Objects
             return _currentState;
         }
 
+        public bool CheckWin (string wordGuess)
+        {
+          bool win;
+          if (_answer == wordGuess)
+          {
+              win = true;
+          }
+          else
+          {
+              win = false;
+              _tries++;
+          }
+          return win;
+        }
     }
 }
